1259956242b98b1b9694f81f01091bfc2829352f
Redplcs <51379496+Redplcs@users.noreply.github.com>
2024-06-26 22:54:26 +0500

    Add package entry header

---
+++ ContentManager/ContentManager.Packages.Tests/PakEntryTests.cs
@@ -0,0 +1,19 @@
+﻿namespace ContentManager.Packages.Tests;
+
+public class PakEntryTests
+{
+	[Fact]
+	public void Read()
+	{
+		using var stream = File.OpenRead("Resources/GlobalRU.pak");
+		using var reader = new BinaryReader(stream);
+		_ = PakHeader.TryRead(reader, out _);
+
+		var entry = PakEntry.Read(reader);
+
+		Assert.Equal("Gfx/Interfaz.txt", entry.Path);
+		Assert.Equal(13u, entry.DataStartPosition);
+		Assert.Equal(44261u, entry.DataSize);
+		Assert.Equal(127846410028233750u, entry.Unknown);
+	}
+}
---
+++ ContentManager/ContentManager.Packages/PakEntry.cs
@@ -0,0 +1,20 @@
+﻿namespace ContentManager.Packages;
+
+internal struct PakEntry
+{
+	public string? Path;
+	public uint DataStartPosition;
+	public uint DataSize;
+	public ulong Unknown;
+
+	public static PakEntry Read(BinaryReader reader)
+	{
+		return new PakEntry()
+		{
+			Path = reader.ReadNullTerminatedString(),
+			DataStartPosition = reader.ReadUInt32(),
+			DataSize = reader.ReadUInt32(),
+			Unknown = reader.ReadUInt64(),
+		};
+	}
+}
